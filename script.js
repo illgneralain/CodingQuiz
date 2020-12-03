4a8ad6cb41b78f6cffc9d962bcd15e34faf57df6
Illgner Alain <illgneralain@gmail.com>
2020-12-03 17:52:40 -0500

    added JSON parse technique learned in class to get Item and push to storage

--- script.js
+++ script.js
@@ -165,6 +165,7 @@ submitScoreBtn.addEventListener("click", function highscore(){
         )
         else
     {
+        var savedHighscores = JSON.parse(localStorage.getItem(""))
 })
 
 // Creating an action for the submit button to store the high score
